9c28f045c8f820ea5e08119268f3bbc657fa895e
sabarishvelu4@gmail.com <sabarishvelu4@gmail.com>
2024-06-21 22:06:37 +0530

    Add project files.

---
+++ Spendsmart/Models/SpendSmartDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Spendsmart.Models
+{
+    public class SpendSmartDBContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+
+        public SpendSmartDBContext(DbContextOptions<SpendSmartDBContext> options)
+            :base(options) 
+        {
+
+        }
+}
+}
